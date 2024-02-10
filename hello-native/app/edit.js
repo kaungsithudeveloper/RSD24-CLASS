a98991a5be624909ce83deaca90bf172aaa67926
kaungsithu <122274867+kaungsithudeveloper@users.noreply.github.com>
2024-02-10 16:27:39 +0630

    Create edit.js

---
+++ hello-native/app/edit.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+
+export default function Edit() {
+	return (
+		<View>
+			<Text>Edit Page</Text>
+		</View>
+	);
+}
